e580e876c47e3889a9c5f1cae98a79db0c8a84e6
NuhuWambali <wambalinuhu@gmail.com>
2023-10-27 14:14:03 +0300

    user and roles fixed,no issue now

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="footer">
     <div><p> © 2023 Darmatrix Viwanja</p> </div>
-    <div class="ms-auto">Powered by&nbsp;<p>DarMatrix</p></div>
+    <div class="ms-auto">Powered by &nbsp;<p>DarMatrix</p></div>
   </footer>
